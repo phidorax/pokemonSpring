568b2cd88c25c8dd9007d17d548138828c49081e
Axel Grard <phidoraxgame@gmail.com>
2023-11-07 13:41:41 +0100

    Add edit and remove routes

--- back/src/main/java/fr/ulco/pokemon/controllers/Routes.java
+++ back/src/main/java/fr/ulco/pokemon/controllers/Routes.java
@@ -5,9 +5,21 @@ public class Routes {
     public static final String GET_POKEMONS_DETAILS = "/pokemons/{id}";
     public static final String GET_POKEMONS_TYPES = "/pokemons/{id}/types";
     public static final String CREATE_POKEMON = "/pokemons/new";
+    public static final String EDIT_POKEMON = "/pokemons/{id}/edit";
+    public static final String DELETE_POKEMON = "/pokemons/{id}/delete";
     public static final String GET_ABILITIES = "/abilities";
+    public static final String GET_ABILITIES_DETAILS = "/abilities/{id}";
+    public static final String CREATE_ABILITY = "/abilities/new";
+    public static final String EDIT_ABILITY = "/abilities/{id}/edit";
+    public static final String DELETE_ABILITY = "/abilities/{id}/delete";
     public static final String GET_TYPES = "/types";
-    public static final String CREATE_TYPE = "/types/new";
     public static final String GET_TYPES_DETAILS = "/types/{id}";
+    public static final String CREATE_TYPE = "/types/new";
+    public static final String EDIT_TYPE = "/types/{id}/edit";
+    public static final String DELETE_TYPE = "/types/{id}/delete";
     public static final String GET_MOVES = "/moves";
+    public static final String GET_MOVES_DETAILS = "/moves/{id}";
+    public static final String CREATE_MOVE = "/moves/new";
+    public static final String EDIT_MOVE = "/moves/{id}/edit";
+    public static final String DELETE_MOVE = "/moves/{id}/delete";
 }
