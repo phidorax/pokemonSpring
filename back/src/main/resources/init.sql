a5a653609a0b83af1a3c85d972505dab870e8126
Axel Grard <phidoraxgame@gmail.com>
2023-12-07 13:48:17 +0100

    Add real pokemon id

--- back/src/main/resources/init.sql
+++ back/src/main/resources/init.sql
@@ -1,4 +1,159 @@
-INSERT INTO pokemon (id, name, hp, attack, defense, speed, special_attack, special_defense)
+CREATE TABLE IF NOT EXISTS ability
+(
+    id
+    bigserial
+    primary
+    key,
+    description
+    varchar
+(
+    255
+),
+    name varchar
+(
+    255
+)
+    );
+
+CREATE TABLE IF NOT EXISTS move
+(
+    id
+    bigserial
+    primary
+    key,
+    accuracy
+    integer,
+    description
+    varchar
+(
+    255
+),
+    name varchar
+(
+    255
+),
+    power integer,
+    pp integer
+    );
+
+CREATE TABLE IF NOT EXISTS pokemon
+(
+    id
+    bigserial
+    primary
+    key,
+    attack
+    integer,
+    defense
+    integer,
+    hp
+    integer,
+    name
+    varchar
+(
+    255
+),
+    special_attack integer,
+    special_defense integer,
+    speed integer
+    );
+
+CREATE TABLE IF NOT EXISTS type
+(
+    id
+    bigserial
+    primary
+    key,
+    name
+    varchar
+(
+    255
+),
+    move_id bigint
+    constraint fkl5dogoa35m0axawwm29gl504e
+    references move
+    );
+
+CREATE TABLE IF NOT EXISTS move_type
+(
+    move_entity_id
+    bigint
+    not
+    null
+    constraint
+    fk89kmp6imjxitg3dwo2obnisuh
+    references
+    move,
+    type_id
+    bigint
+    not
+    null
+    constraint
+    uk_95fxjjdu7qcsw18q4vsw0bs1w
+    unique
+    constraint
+    fkds642av1q97m809fm8acybbet
+    references
+    type
+);
+
+CREATE TABLE IF NOT EXISTS pokemon_type
+(
+    pokemon_id
+    bigint
+    not
+    null
+    constraint
+    fkjii6ffk1wg2utlg0ex7cg3uqy
+    references
+    pokemon,
+    type_id
+    bigint
+    not
+    null
+    constraint
+    fkn1bw91wtw3yq2ai9hpvb4pa0o
+    references
+    type
+);
+
+CREATE TABLE IF NOT EXISTS users
+(
+    username
+    varchar
+(
+    50
+) not null primary key,
+    password varchar
+(
+    500
+) not null,
+    enabled boolean not null
+    );
+
+CREATE TABLE IF NOT EXISTS authorities
+(
+    username
+    varchar
+(
+    50
+) not null,
+    authority varchar
+(
+    50
+) not null,
+    constraint fk_authorities_users foreign key
+(
+    username
+) references users
+(
+    username
+)
+    );
+
+CREATE UNIQUE INDEX ix_auth_username on authorities (username, authority);
+
+INSERT INTO pokemon (id, name, hp, attack, defense)
 VALUES (25, 'Pikachu', 35, 55, 40, 90, 50, 50),
        (4, 'Charmander', 39, 52, 43, 65, 60, 50),
        (7, 'Squirtle', 44, 48, 65, 43, 50, 64);
